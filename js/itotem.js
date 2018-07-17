727adc771be04844ce8a0c554df0df0800791565
Josh Stuible <shrewdlove@gmail.com>
2018-07-16 22:31:30 -0700

    Added Client sectino

--- js/itotem.js
+++ js/itotem.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
         prevArrow:"<div class='a-left control-c prev slick-prev' ></div>",
         nextArrow:"<div class='a-right control-c next slick-next' ></div>"
     });
+
+    $('.landing-clients').slick({
+        slidesToShow: 5,
+        prevArrow:"<div class='a-left control-c prev slick-prev client-arrow prev' ></div>",
+        nextArrow:"<div class='a-right control-c next slick-next client-arrow next' ></div>"
+    });
+    
   });
