3f591815d307bc54a109e47da4a3df0c8a923adf
Josh Stuible <shrewdlove@gmail.com>
2018-07-17 22:20:27 -0700

    Enabled autoplay

--- js/itotem.js
+++ js/itotem.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.hero-carousel').slick({
         slidesToShow: 1,
         arrows: true,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 5000,
         dots: true,
         dotsClass: 'hero-dots',
